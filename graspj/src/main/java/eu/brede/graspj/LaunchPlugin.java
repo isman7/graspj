5977874fb3d76a4af69248ed3a9749e8bae0b1bf
Ismael Benito <isman7@gmail.com>
2015-04-30 10:53:06 +0200

    Commented lines about testing correct Plugin launch

--- graspj/src/main/java/eu/brede/graspj/LaunchPlugin.java
+++ graspj/src/main/java/eu/brede/graspj/LaunchPlugin.java
@@ -1,11 +1,19 @@
 package eu.brede.graspj;
 
 import ij.plugin.PlugIn;
+//import fiji.Debug;
+
+/*import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.*;*/
 
 public class LaunchPlugin implements PlugIn {
 	
 	@Override
 	public void run(String arg) {
 		LaunchStandalone.main(new String[] {arg});
+		//IJ.showMessage("My_Plugin","Hello world!");
 	}
 }
